a25b4fe4bb6d74c5773e0d282b28eb2c857fe875
drstsny <drstsny@gmail.com>
2025-02-11 13:59:30 +0700

    first commit

---
+++ src/main/java/com/staging/staging_juwangi/StagingJuwangiApplication.java
@@ -0,0 +1,13 @@
+package com.staging.staging_juwangi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StagingJuwangiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StagingJuwangiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/staging/staging_juwangi/StagingJuwangiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.staging.staging_juwangi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StagingJuwangiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
